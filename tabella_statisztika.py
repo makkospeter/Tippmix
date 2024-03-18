0a8ef9af1a4db5c2048a4752acaef1921607a07b
Ikker Benjámin <ikker.benjamin@students.jedlik.eu>
2024-03-18 13:34:15 +0100

    La liga hozzáadva a statitsztikához

--- tabella_statisztika.py
+++ tabella_statisztika.py
@@ -1,6 +1,7 @@
 
-from tabella_class import Tabella
+from tabella_class import Tabella, Tabella2
 tabella_adatai : list[Tabella] = []
+tabella2_adatai : list[Tabella2] = []
 
 def beolvasas(faljnev):
     file = open(faljnev, 'r', encoding='utf8')
@@ -9,8 +10,15 @@ def beolvasas(faljnev):
         tabella_adatai.append(Tabella(sor))
     file.close()
 
+def beolvasas2(faljnev):
+    file = open(faljnev, 'r', encoding='utf8')
+    file.readline()
+    for sor in file:
+        tabella2_adatai.append(Tabella2(sor))
+    file.close()
+
 def bekeres():
-    print('Premier Legaue csapatelemző')
+    print('Premier League csapatelemző')
     bekeres = input('Csapat neve: ')
     for cs in tabella_adatai:
         if bekeres in cs.csapat:
@@ -25,4 +33,35 @@ def bekeres():
             print(f'\tLegdrágább játékos: {cs.jatekos}')
             break
     if bekeres not in cs.csapat:
-        print('A keresett csapat nem található :(')
+        print('A keresett csapat nem található!')
+
+
+def csapat_listazas():
+    print('A Premier League Jelenlegi csapatai:')
+    for cs in tabella_adatai:
+        print(f'\t{cs.helyezes} - {cs.csapat}')
+
+
+def bekeres2():
+    print('La Liga csapatelemző')
+    bekeres = input('Csapat neve: ')
+    for cs in tabella2_adatai:
+        if bekeres in cs.csapat:
+            print(f'\tNév: {cs.csapat} ')
+            print(f'\tHelyezés: {cs.helyezes}.')
+            print(f'\tMérkőzések (W/D/L): {cs.merkozesek} Mérkőzés, {cs.gyozelmek}/{cs.dontetlenek}/{cs.vesztesegek}')
+            print(f'\tPontok: {cs.pontok}')
+            print(f'\tÁtlagos gólok száma: {cs.golok}')
+            print(f'\tÁtlagos szögletszám: {cs.szoglet}')
+            print(f'\tÁtlagos sárgalap szám: {cs.sargalap}')
+            print(f'\tÁtlagos piroslap szám: {cs.piroslap}')
+            print(f'\tLegdrágább játékos: {cs.jatekos}')
+            break
+    if bekeres not in cs.csapat:
+        print('A keresett csapat nem található!')
+
+
+def csapat_listazas2():
+    print('A Premier League Jelenlegi csapatai:')
+    for cs in tabella2_adatai:
+        print(f'\t{cs.helyezes} - {cs.csapat}')
