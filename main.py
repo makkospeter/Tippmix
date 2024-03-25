c817d3aab4b2ad86966058bf42d0f021d77bbab0
Ikker Benjámin <ikker.benjamin@students.jedlik.eu>
2024-03-25 13:12:41 +0100

    main 1., 3. menupont mukodik

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from os import system
 from tabella_statisztika import *
-from odds import keres_meccs, rand_meccs
+import odds
+import tabella_main
 beolvasas('premier_tabella.csv')
 beolvasas2('laliga_tabella.csv')
 
@@ -12,39 +13,12 @@ def main_():
             case '0':
                 pass
             case '1':
-                    while True:
-                        melyik = 0
-                        system('cls')
-                        while melyik != 1 and melyik != 2:
-                            melyik = int(input('Keresni szeretne a csapatok között vagy véletlen felajánlást kíván?(Keres - 1, Véletlen - 2):  '))
-                            match melyik:
-                                case 1:
-                                    keres_meccs()
-                                case 2:  
-                                    rand_meccs()
+                    system('cls')
+                    odds.main()
             case '2':
                 pass
-            case '3':
-                    while True:
-                        system('cls')
-                        print('1 - Premier League jelenlegi csapatai helyezés sorrendben')
-                        print('2 - Premier League csapat elemző')
-                        print('3 - La Liga jelenlegi csapatai helyezés sorrendben')
-                        print('4 - La Liga csapat elemző')
-                        print('5 - Vissza')
-                        valasztas = input('Választás: ')
-                        match valasztas:
-                            case '1':
-                                csapat_listazas()
-                            case '2':
-                                bekeres()
-                            case '3':
-                                csapat_listazas2()
-                            case '4':
-                                bekeres2()
-                            case '5':
-                                break
-                        input('Tovább...')
+            case '3':   
+                    tabella_main.tabella_main_fugv()
             case '4':
                 pass
                 
--- tabella_main.py
+++ tabella_main.py
@@ -1,23 +1,30 @@
-# from tabella_statisztika import *
-# def tabella_main_fugv():
-#     beolvasas('premier_tabella.csv')
-#     beolvasas2('laliga_tabella.csv')
-#     while True:
-#         match tabella_menu():
-#             case '1':
-#                 csapat_listazas()
-#             case '2':
-#                 bekeres()
-#             case '3':
-#                 csapat_listazas2()
-#             case '4':
-#                 bekeres2()
-#         input('Vissza...')
+from tabella_statisztika import *
+from os import system
+def tabella_main_fugv():
+    beolvasas('premier_tabella.csv')
+    beolvasas2('laliga_tabella.csv')
+    while True:
+        system('cls')
+        match tabella_menu():
+            case '1':
+                csapat_listazas()
+            case '2':
+                bekeres()
+            case '3':
+                csapat_listazas2()
+            case '4':
+                bekeres2()
+            case '5':
+                break
+        input('Vissza...')
+        system('cls')
 
-# def tabella_menu() -> str:
-#     print('1 - Premier League jelenlegi csapatai helyezés sorrendben')
-#     print('2 - Premier League csapat elemző')
-#     print('3 - La Liga jelenlegi csapatai helyezés sorrendben')
-#     print('4 - La Liga csapat elemző')
-#     return input('Választás: ')
-# tabella_main_fugv()
+def tabella_menu() -> str:
+    print('1 - Premier League jelenlegi csapatai helyezés sorrendben')
+    print('2 - Premier League csapat elemző')
+    print('3 - La Liga jelenlegi csapatai helyezés sorrendben')
+    print('4 - La Liga csapat elemző')
+    print('5 - Vissza')
+    return input('Választás: ')
+if __name__ == "__main__":
+    tabella_main_fugv()
